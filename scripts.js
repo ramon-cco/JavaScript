be036f5c47045c1972202b4f66f2dbc8ec86aa59
Ramon Oliveira <ramoncco@outlook.com>
2021-05-25 10:40:33 -0300

    criação do arquivo tipos de dados alteração no arquivo script para o padrão

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-// aqui eu tenho um comentário em linha.
-
-/* aqui tenho um comentário em bloco
-
-O código abaixo escrete alguma mensagem no devtools
-*/
-
-console.log('Bem vindo ao Starter') // alguma coisa
+console.log("teste")
---
+++ tipos_dados.js
@@ -0,0 +1,74 @@
+// Cadeia de caracteres
+
+/* 
+"" aspas duplas
+'' aspas simples
+`` template literals ou template strings
+*/
+
+console.log(` String--> Ramon ${25*25}`) 
+
+/* number
+* number
+
+33 inteiros
+12.5 reais-floar
+Nan not a number
+Infinity 
+*/
+console.log('numbers:')
+console.log( -33)
+
+/*
+somente 2 valores
+true // verdadeiro
+false // falso
+
+*/
+
+console.log(false)
+
+
+/*
+ *undefined
+    * indefinido
+
+ * null
+    * nulo
+    * objeto que não possui nada dentro
+    * diferente de indefinido
+
+*/
+
+console.log(undefined)
+
+/*
+ * Object
+    *objeto
+    * Propriedades / Atributos
+    * Funcionalidades / Métodos
+    * 
+    {propriedade: "valor"}
+*/
+
+console.log({
+    name:"ramon",
+    idade: 36,
+    andar: function(){
+        console.log("andar")
+    }
+})
+
+/*
+ * Array (Vetores)
+    * uma lista
+    * Agrupamento de dado
+    * 
+    ["Mayk", 36]
+*/
+console.log([
+    "mayk",
+    36,
+    "ovos",
+    3
+])
