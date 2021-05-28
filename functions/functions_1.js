96bbb2750e49712ea5748ca8e336034e9180841b
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2021-05-27 23:55:49 -0300

    aula d funções, introdução e utilização. add files

---
+++ functions/functions_1.js
@@ -0,0 +1,22 @@
+// criar um aplicativo de frases motivacionais
+
+// declaretion - declaração de função
+// function statement
+
+function createPhrases(){
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('REvisão é mãe do aprendizado')
+}
+
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+
+createPhrases()
+
+console.log('Fim do programa')
+
+
+// funções agrupam codigo, reutilizam codigo.
