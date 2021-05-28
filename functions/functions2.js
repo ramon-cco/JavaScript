e8d6e37c5d0f22166cdfff92342838dd91c4d55a
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2021-05-28 15:45:57 -0300

    function scope e hosting,

---
+++ functions/functions2.js
@@ -0,0 +1,22 @@
+// function expression
+// function anonumous
+
+
+//parâmentros da função (parameters)
+const sum = function(number1, number2){
+    let total = number1 + number2
+    return total
+}
+
+// arguments - argumentos
+
+let number1 = 34
+let number2 = 25
+
+//  console.log(`o número 1 é ${number1}`)
+//  console.log(`o número 2 é ${number2}`)
+ console.log(`A some é  ${sum(number1, number2)}`)
+ //fora do escopo
+  console.log(total)
+ 
+ 
