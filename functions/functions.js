0d31a1d178708644ae4c6703cf4563669df596ba
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2021-05-28 15:21:20 -0300

    working with functions

--- functions/functions.js
+++ functions/functions.js
@@ -4,7 +4,19 @@
 
 //parâmentros da função (parameters)
 const sum = function(number1, number2){
-    console.log(number1 + number2)
+    let total = number1 + number2
+    return total
 }
 
-sum (2, 3) // arguments - argumentos
+// arguments - argumentos
+
+let number1 = 34
+let number2 = 25
+
+//  console.log(`o número 1 é ${number1}`)
+//  console.log(`o número 2 é ${number2}`)
+ console.log(`A some é  ${sum(number1, number2)}`)
+ //fora do escopo
+  console.log(total)
+ 
+ 
