b2f0d63d126397c9c6f31dbfe8ea805bee6d5fe3
Ramon Oliveira <ramoncco@outlook.com>
2021-06-01 14:38:40 -0300

    Create expressions_operations.js
    create file

---
+++ functions/expressions_operations.js
@@ -0,0 +1,22 @@
+/*
+Expressões e Operadores
+
+    - Expressions
+    - Operators
+        binary
+        unary
+        ternary
+*/
+
+let number = 1
+
+//operador binary
+console.log(number + 1)
+
+//operador unary
+
+console.log(--number)
+
+//operador ternary (one case only)
+
+console.log(false? 'alo' : 'nada')
