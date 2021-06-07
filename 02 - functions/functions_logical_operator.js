6e941189fa66b703328e2196b8796eb37b8e5f62
Ramon Oliveira <ramoncco@outlook.com>
2021-06-07 16:23:42 -0300

    Create functions_logical_operator.js
    create file

---
+++ 02 - functions/functions_logical_operator.js
@@ -0,0 +1,17 @@
+/* 
+    Operadores lógicos (logical operator)
+
+    2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+*/
+
+let pao = false
+let queijo = true
+
+// AND && ( os dois tem que ser verdadeiros)
+// console.log(pao && queijo)
+
+// OR || ( um precisa ser verdadeiro)
+// console.log(pao || queijo)
+
+// NOT ! ( negação)
+console.log(!pao)
