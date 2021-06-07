184bc044edd35ea5cc403b4ab0a1185247659db6
Ramon Oliveira <ramoncco@outlook.com>
2021-06-07 16:23:11 -0300

    Create functions_operations_comparation.js
    create file

---
+++ 02 - functions/functions_operations_comparation.js
@@ -0,0 +1,43 @@
+/*
+    Operadores de comparação
+
+
+ Irá comparar valores e retornar um Boolean como resposta á comparação
+ */
+
+let one = 1
+let two = 2
+
+/* == igual a */
+// console.log( two == 1)
+// console.log( one == "1")
+
+/* != diferente de */ 
+// console.log(one != two)
+// console.log(one != 1)
+// console.log(one != "1")
+
+/* === Estritamente igual a */
+
+// console.log( one === "1")
+// console.log( one === 1)
+
+
+/* !== Estritamente de */
+// console.log( two !== "2")
+// console.log( two !== 2)
+
+/* > maior que */
+// console.log(one > two)
+
+/* >= Maior igual a*/
+// console.log(one >= 1)
+// console.log(two >= 1)
+
+/* < menor que */
+// console.log(one < two)
+
+/* <= Menor igual a*/
+console.log(one <= two)
+console.log(two <= 1)
+console.log(two <= 0)
