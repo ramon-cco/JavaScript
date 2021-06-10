149d917a6900140614a960baa30bc26ed22203bc
Ramon Oliveira <ramoncco@outlook.com>
2021-06-10 16:34:08 -0300

    Create functions_ternary_operator.js
    create file

---
+++ 02 - functions/functions_ternary_operator.js
@@ -0,0 +1,30 @@
+/* 
+    Operadores Condicional (Ternário)
+
+    Dependendo da condição, nós recebemos valores diferentes
+
+    Condição então valor 1 se não valor 2
+*/
+
+//Condition ? value1 : value2
+
+//Exemplos
+
+// Café da manha top
+// let pao = true;
+// let queijo = false;
+
+/* se pao ou queijo um for verdadeiro => café top, se forem ambos 
+falsos => café ruim */
+
+// const niceBreakFast =  pao || queijo ? 'Café top' : 'Café ruim'
+ // console.log(niceBreakFast)
+
+ //_______________________________________________________________
+
+//Maior de 18
+
+let age = 16
+const canDrive = age >= 18 ? 'can drive' : " can't drive"
+
+console.log(canDrive)
