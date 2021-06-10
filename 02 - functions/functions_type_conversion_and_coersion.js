08e18afa8245815cda20294c645fdbe4b13268c2
Ramon Oliveira <ramoncco@outlook.com>
2021-06-10 17:09:53 -0300

    Update functions_type_conversion_and_coersion.js
    create files

--- 02 - functions/functions_type_conversion_and_coersion.js
+++ 02 - functions/functions_type_conversion_and_coersion.js
@@ -18,4 +18,23 @@ console.log(Number('9') + 5)
     NaN
 */    
 
-console.log (false ? 'verdadeiro' : 'falso')
+console.log (false ? 'verdadeiro' : 'falso'),
+
+/*
+    TRUTHY
+    Quando um valor é considerado  TRUE em contextos onde um booleano é
+    obrigatório (condicionais e loops)
+
+   true
+   {}
+   []
+   1
+   3.23
+   "0"
+   "false"
+   -1
+    Infinity
+   -Infinity
+*/    
+
+console.log(true ? 'verdadeiro' : 'falso')
