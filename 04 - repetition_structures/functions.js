e0a76d2ac703478b7fbb87bc9612962bc9b15f6f
Ramon Oliveira <ramoncco@outlook.com>
2021-06-14 14:44:10 -0300

    Update functions.js
    inputting information about the files and how they are used

--- 04 - repetition_structures/functions.js
+++ 04 - repetition_structures/functions.js
@@ -1,48 +1,7 @@
 /*
-Estrtura de repetição
-    FOR
 
-    for ([inicialização]; [condição]; [expressão final])
-   declaração
-
-   ex: 
-   for (var i = 0; i < 9; i++) {
-   console.log(i);
-   // more statements
-}
-
+     Arquivo utilizado para aprendizado, dados inseridos nos respectivos arquivos
+   .js na pasta do módulo
+   
 */
 
-//Padrão
-for(let i=0; i <10; i++){
-    console.log(i)
-}
-
-console.log("++++++++++++++++++(º____º)++++++++++++++++++")
-
-/*for com opcionais (Break)
-    Break ==> utilizado para parar o codigo em certo ponto no caso do exemplo a 
-baixo ele para no 50
-*/
-
-for(let n=100; n > 0; n--){
-    if(n === 50){
-        break
-    }
-    
-    console.log(n)
-}
-
-console.log("++++++++++++++++++(º____º)++++++++++++++++++")
-
-/*for com opcionais (continue)
-    Continue ==> utilizado para pular um ponto no codigo, exemplo a 
-baixo ele pula o 5 e contnua 
-*/
-for(let m=10; m > 0; m--){
-    if(m === 5){
-        continue
-    }
-    
-    console.log(m)
-}
