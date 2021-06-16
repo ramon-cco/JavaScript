4d9cca2dddcb3251dded9b1b0be16e94e2ffb9dd
Ramon Oliveira <ramoncco@outlook.com>
2021-06-16 16:37:23 -0300

    Update functions.js
    ajust

--- 05- Exercises/celsius_fahrenheit/functions.js
+++ 05- Exercises/celsius_fahrenheit/functions.js
@@ -48,40 +48,41 @@
     /* 2º versão - Professor*/
 
     function transformDegree(degree) {
-        const celsiusExists = degree.toUpperCase().includes('C')
-        const fahrenheitExists = degree.toUpperCase().includes('F')
+      const celsiusExists = degree.toUpperCase().includes('C')
+      const fahrenheitExists = degree.toUpperCase().includes('F')
 
-        //fluxo de erro
-        if(!celsiusExists && !fahrenheitExists){
-          throw new Error('Grau não identificado')
-        }
+      //fluxo de erro
+      if(!celsiusExists && !fahrenheitExists){
+        throw new Error('Grau não identificado')
+      }
 
-        //fluxo ideal, F -> C
-        let updatedDegree = Number(degree.toUpperCase().replace("F",""));
-        //number- numero criado  // toUpperCase -  converte para maiusculas // replace("C","") - remove o C e coloca no lugar vazio ""
-        
-        let formula = fahrenheit => (fahrenheit - 32) * 5/9
-        //arrow function -(param1, param2, …, paramN) => expression
-        // equivalente a: => { return expression; }
+      //fluxo ideal, F -> C
+      let updatedDegree = Number(degree.toUpperCase().replace("F",""));
+      //number- numero criado  // toUpperCase -  converte para maiusculas // replace("C","") - remove o C e coloca no lugar vazio ""
+      
+      let formula = fahrenheit => (fahrenheit - 32) * 5/9
+      //arrow function -(param1, param2, …, paramN) => expression
+      // equivalente a: => { return expression; }
 
-        let degreeSign = 'C' //varaivel com a letra
+      let degreeSign = 'C' //varaivel com a letra
 
-        //fluxo alternativo 
-        if(celsiusExists){
-          updatedDegree = Number(degree.toUpperCase().replace("C", ""));
-          formula = celsius => celsius * 9/5 + 32
-          degreeSign = 'F'
-        }
+      //fluxo alternativo 
+      if(celsiusExists){
+        updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = celsius => celsius * 9/5 + 32
+        degreeSign = 'F'
+      }
 
-        return formula(updatedDegree) + degreeSign // retornando o resultado
-        // com a letra equivalente aos grau
+      return formula(updatedDegree) + degreeSign // retornando o resultado
+      // com a letra equivalente aos grau
 
+  }
+    //eecutando com o try
+    try{
+      console.log(transformDegree('10C'))
+      console.log(transformDegree('50F'))
+      console.log(transformDegree('50z'))
+    } catch (error){
+      console.log(error.message)
     }
 
-      try{
-        console.log(transformDegree('10C'))
-        console.log(transformDegree('50F'))
-        console.log(transformDegree('50z'))
-      } catch (error){
-        console.log(error.message)
-      }
