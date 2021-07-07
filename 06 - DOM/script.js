aefbb5fbcdbe6d65408aed84462d28ab5bd8f5ba
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2021-07-06 21:55:32 -0300

    add files

---
+++ 06 - DOM/script.js
@@ -0,0 +1,20 @@
+/* Conceitos basico do DOM com JS */
+
+//getElmentById()
+const element = document.getElementById('blog-title')
+console.log(Element)
+
+
+//getElementByClassName() - Busca elementos pelo nome da classe e os mostra em um HTMLCollection
+const className = document.getElementsByClassName('one')
+console.log(className)
+
+
+//getElementByTagName() - Busca elementos pelo nome da tag e os mostra em um HTMLCollection
+const tag = document.getElementsByTagName('meta')
+console.log(tag)
+
+//querySelector() - busca pelo seletor 
+const query = document.querySelector('.one')
+console.log(query)
+
